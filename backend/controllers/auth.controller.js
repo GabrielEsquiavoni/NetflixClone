85d5f4de564ed3c200558fa148b82b9b2be4aa2f
GabrielEsquiavoni <gabrielazevedo.2004@gmail.com>
2025-03-26 16:15:38 -0300

    Auth Controller Funcional

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import {User} from '../models/user.model.js'
+import bcryptjs from 'bcryptjs';
+import { generateTokenAndSetCookie } from '../utils/generateToken.js';
 
 export async function signup(req, res){
     try{
@@ -30,18 +32,30 @@ export async function signup(req, res){
             return res.status(400).json({success:false,message:"Username already exists"})
         }
         
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
         const PROFILE_PICS = [];
 
         const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
 
         const newUser = new User ({
             email,
-            password,
+            password: hashedPassword,
             username,
             image
         })
 
-        await newUser.save()
+        generateTokenAndSetCookie(newUser._id,res);
+        await newUser.save();
+        // Remove senha da resposta
+            res.status(201).json({
+            success: true,
+            user:{
+            ...newUser._doc,
+             password:"",
+            },
+         });
     } catch (error){
         console.log("Error in Signup Controller",error.message)
         res.status(500).json({success:false,message:"internal server error"})
@@ -49,9 +63,45 @@ export async function signup(req, res){
 }
 
 export async function login(req, res){
-    res.send('Login route');
+    try{
+        const {email,password} = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({success: false, message: "All fields are required"});
+        }
+
+        const user = await User.findOne({email: email})
+        if(!user){
+            return res.status(404).json({success: false, message: "Invalid credentials"});
+        }
+
+        const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+        if(!isPasswordCorrect){
+            return res.status(400).json({success: false, message: "Invalid credentials"});
+        }
+
+        generateTokenAndSetCookie(user._id,res);
+
+        res.status(200).json({
+            success: true,
+            user: {
+                ...user._doc,
+                password: ""
+            }
+        })
+    } catch (error) {
+        console.log("Error in Login Controller", error.message);
+        res.status(500).json({success: false, message: "Internal server error"});
+    }
 }
 
 export async function logout(req, res){
-    res.send('Logout route');
+    try{
+        res.clearCookie("jwt-netflix");
+        res.status(200).json({success: true, message: "Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({success: false, message: "Internal server error"});
+    }
 }
