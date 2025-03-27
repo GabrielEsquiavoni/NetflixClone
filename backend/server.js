eb59cf02d73f77a8b89d7806a1de97d3752b6cb3
GabrielEsquiavoni <gabrielazevedo.2004@gmail.com>
2025-03-27 17:17:27 -0300

    Middleware configurado

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,27 @@
 // const express = require('express); // CommonJS
 import express from 'express'; // esm
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
 import movieRoutes from './routes/movie.route.js';
 import tvRoutes from './routes/tv.route.js'
 
+
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
+import { protectRoute } from './middleware/protectRoute.js';
+
 
 const app = express();
 
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // permite req.body
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
   console.log('server is listening at http://localhost:' + PORT);
