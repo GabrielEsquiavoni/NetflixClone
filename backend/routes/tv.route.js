9d22b270fec1d768b8c81cf7ae6300afcd3749ec
GabrielEsquiavoni <gabrielazevedo.2004@gmail.com>
2025-03-27 16:38:28 -0300

    Rotas e Controle de TVShows funcionando

---
+++ backend/routes/tv.route.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import {getTrendingTv, getTvsByCategory, getTvTrailers, getTvDetails, getSimilarTvs } from '../controllers/tv.controller.js';
+
+
+const router = express.Router();
+
+
+
+router.get("/trending", getTrendingTv);
+router.get("/:id/trailers", getTvTrailers);
+router.get("/:id/details", getTvDetails);
+router.get("/:id/similar", getSimilarTvs);
+router.get("/:category", getTvsByCategory);
+
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express'; // esm
 
 import authRoutes from './routes/auth.route.js';
 import movieRoutes from './routes/movie.route.js';
+import tvRoutes from './routes/tv.route.js'
 
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
@@ -15,6 +16,7 @@ app.use(express.json()); // permite req.body
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/tv", tvRoutes);
 
 app.listen(PORT, () => {
   console.log('server is listening at http://localhost:' + PORT);
